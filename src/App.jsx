ca31705920ca660b994053816628c324315657a9
Donielle Berg <donielle.berg@gmail.com>
2018-04-30 22:59:51 -0700

    Change to hashRouter for gh-pages and create-react-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom'
 import IconSettings from 'design-system-react/components/icon-settings';
 
 import './App.css';
